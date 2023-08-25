fe4f77022ee1a2b5b74baf9b3d636f05599cbe5e
arnulfotos <manuelleon1996@gmail.com>
2023-08-25 14:07:23 -0700

    arreglo final

--- main.js
+++ main.js
@@ -2,47 +2,43 @@ import { calcNoc } from "./calcNoc";
 import { calcDia } from "./calcDia";
 import { fecha } from "./dia";
 
-
 const btnCotizar = document.getElementById("calc");
-const cotizacion = document.getElementById("cotizacion")
-
-
+const cotizacion = document.getElementById("cotizacion");
+const totalInput = document.getElementById("total");
 
 let printCot = (cot) => {
-    let p = document.createElement('p')
-    let text = document.createTextNode(cot)
-    /* let br = document.createElement('br') 
+  let p = document.createElement("p");
+  let text = document.createTextNode(cot);
+  /* let br = document.createElement('br') 
     let hr = document.createElement('hr')*/
-    p.appendChild(text)
+  p.appendChild(text);
 
-    cotizacion.appendChild(p)
-    /* cotizacion.appendChild(br) 
+  cotizacion.appendChild(p);
+  /* cotizacion.appendChild(br) 
     cotizacion.appendChild(hr)*/
-
-
-}
+};
 
 fecha();
-btnCotizar.addEventListener('click', (e) => {
-    
-const dias = document.getElementById("entrada-general").value;
-const camping = document.getElementById("camping").value;
-const numDePersonas = document.getElementById("num-personas").value;
-const numDeAutos = document.getElementById("auto").value;
+btnCotizar.addEventListener("click", (e) => {
+  const dias = document.getElementById("entrada-general").value;
+  const camping = document.getElementById("camping").value;
+  const numDePersonas = document.getElementById("num-personas").value;
+  const numDeAutos = document.getElementById("auto").value;
 
+  let cotCamping = calcNoc(camping, numDePersonas, numDeAutos);
+  let cotEGeneral = calcDia(numDePersonas, dias);
+  let calcTotal = ` El total a pagar es de ${
+    cotCamping[0] + cotEGeneral[0]
+  } pesos`;
+  printCot(cotCamping[1]);
+  printCot(cotEGeneral[1]);
+  printCot(calcTotal);
+  let br = document.createElement("br");
+  let hr = document.createElement("hr");
+  cotizacion.appendChild(br);
+  cotizacion.appendChild(hr);
 
-    
-    let cotCamping = calcNoc(camping, numDePersonas, numDeAutos)
-    let cotEGeneral = calcDia(numDePersonas, dias)
-    let calcTotal = ` El total a pagar es de ${cotCamping[0] + cotEGeneral[0]} pesos`
-    printCot(cotCamping[1])
-    printCot(cotEGeneral[1])
-    printCot(calcTotal)
-    let br = document.createElement('br') 
-    let hr = document.createElement('hr')
-    cotizacion.appendChild(br) 
-    cotizacion.appendChild(hr)
+  totalInput.value = cotCamping[0] + cotEGeneral[0];
 
-    e.preventDefault();
-})
-
+  e.preventDefault();
+});
