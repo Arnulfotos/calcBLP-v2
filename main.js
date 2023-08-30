3a3aaa9d471cdf177b0edec42f64743a7db004a4
arnulfotos <manuelleon1996@gmail.com>
2023-08-29 19:13:07 -0700

    Funcion de imagen anadida

--- main.js
+++ main.js
@@ -3,16 +3,28 @@ import { calcDia } from "./calcDia";
 import { fecha } from "./dia";
 import { printCot, endCot } from "./DOMoperators";
 import { cotizarCabanas } from "./calcCab";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+import * as htmlToImage from 'html-to-image';
+import download from "downloadjs";
+
+
+
 
 const btnCotizar = document.getElementById("calc");
+const img = document.getElementById("img");
 const cotizacion = document.getElementById("cotizacion");
 const totalInput = document.getElementById("total");
-const deleteCot = document.getElementsByClassName
 
-const dias = document.getElementById("entrada-general").value = 1
-const camping = document.getElementById("camping").value = 1
+/* 
+const wrapper = document.getElementsByClassName('form-wrapper')[0]
+const deleteCot = document.querySelector('delete'); */
+
+
+/* const dias = document.getElementById("entrada-general").value = 1
+const camping = document.getElementById("camping")
+camping.value = 1
 const numDePersonas = document.getElementById("num-personas").value = 5
-const numDeAutos = document.getElementById("auto").value = 1
+const numDeAutos = document.getElementById("auto").value = 1 */
 
 let addDelete = (domElement) => {
   let button = document.createElement("button");
@@ -51,7 +63,7 @@ let cotizarCampingyEntrada = (e) => {
 printCot(cotCamping[1], cotizacion);
     printCot(cotEGeneral[1], cotizacion);
   printCot(calcTotal, cotizacion);
-  addDelete(cotizacion);
+ /* addDelete(cotizacion); */
  endCot(cotizacion);
   
 
@@ -65,11 +77,43 @@ printCot(cotCamping[1], cotizacion);
 
 fecha();
 btnCotizar.addEventListener("click", (e) => {
-  cotizarCampingyEntrada(e);
-  cotizarCabanas(e);
+
+  if (camping.value) {
+    cotizarCampingyEntrada(e);
+
+  } else {
+    cotizarCabanas(e);
+
+  }
+
 
   
 });
+
+camping.addEventListener('blur', ()=>{
+  console.log('hola')
+})
+
+img.addEventListener("click", (e) => {
+
+  /* htmlToImage.toJpeg(wrapper, { quality: 0.95 })
+  .then(function (dataUrl) {
+    var link = document.createElement('a');
+    link.download = 'my-image-name.jpeg';
+    link.href = dataUrl;
+    link.click();
+  }); */
+  /* const wrapper = document.getElementsByClassName('form-wrapper')[0] */
+  const imgBody = document.getElementById('img-body')
+  htmlToImage.toPng(imgBody, {})
+  .then(function (dataUrl) {
+    download(dataUrl, 'my-node.png');
+  });
+
+  e.preventDefault();
+
+})
+
 
 
 
